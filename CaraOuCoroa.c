4a62567758ee0b3c6e5376404466d131a192a7a7
MarcelaMagris <95323963+MarcelaMagris@users.noreply.github.com>
2023-10-15 00:48:11 -0300

    Update CaraOuCoroa.c

--- CaraOuCoroa.c
+++ CaraOuCoroa.c
@@ -10,9 +10,13 @@ void BoasVindas(){
   printf("-----------------------------------\n");
   printf("------------BOAS-VINDAS------------\n");
   printf("-----------------------------------\n");
+  sleep(1);
   printf("-Escolha 1 para Cara---------------\n");
+  
   printf("-Escolha 2 para Coroa--------------\n");
+ 
   printf("-Escolha 0 para Sair---------------\n");
+  sleep(1);
 }
 
 int LeEscolha(){
@@ -30,9 +34,13 @@ int LeEscolha(){
 
 void PrintaEscolha(int n){
   if(n==1){
+    
     printf("Voce escolheu Cara\n");
+    sleep(1);
   }else if(n==2){
+    
     printf("Voce escolheu Coroa\n");
+    sleep(1);
   }
 }
 
@@ -51,28 +59,36 @@ int Sorteia(){
 void PrintaSorteio(int n){
   if(n==Cara){
     printf("Cara foi o lado sorteado!\n");
+    sleep(1);
   }else if (n==Coroa){
     printf("Coroa foi o lado sorteado!\n");
+    sleep(1);
   }
 }
 
 int ContaVitoria(int escolha, int sorteio){
   if(escolha==sorteio){
     printf("Parabéns, você ganhou, você escolheu ");
+    sleep(1);
     if(escolha==Cara){
       printf("Cara, e foi sorteado Cara!\n");
+      sleep(1);
       return 1;
     }else if(escolha==Coroa){
       printf("Coroa, e foi sorteado Coroa!\n");
+      sleep(1);
       return 1;
     }
   }else if(escolha!=sorteio){
     printf("Desculpa, você escolheu ");
+    sleep(1);
       if(escolha==Cara){
         printf("Cara, e foi sorteado Coroa!\n");
+        sleep(1);
         return 0;
       }else if(escolha==Coroa){
         printf("Coroa, e foi sorteado Cara!\n");
+        sleep(1);
         return 0;
       }
     
@@ -86,14 +102,21 @@ void PrintaVitoria(int n){
 void TelaFinalizacao(){
   printf("-----------------------------------\n");
   printf("----GOSTARIA DE JOGAR DE NOVO?-----\n");
+  sleep(1);
   printf("-----------------------------------\n");
   printf("-Escolha 1 para Cara---------------\n");
   printf("-Escolha 2 para Coroa--------------\n");
   printf("-Escolha 0 para Sair---------------\n");
+  sleep(1);
+}
+
+void PrintaTentativas(int n, int m){
+  double media= (1.0*m)/n;
+  printf("Em %d tentativas! Com media de vitorias de %.2f",n, media);
 }
 
 int main(){
-  int escolha=1, joga, vitorias=0, conta=0;
+  int escolha=1, joga, vitorias=0, conta=0, tentativas=0;
   BoasVindas();
   while(escolha !=0){
     srand(time(NULL));
@@ -109,7 +132,11 @@ int main(){
     vitorias = vitorias + conta;
     PrintaVitoria(vitorias);
     TelaFinalizacao();
-    }
+    tentativas++;
+    
+  }
   PrintaVitoria(vitorias);
+  PrintaTentativas(tentativas, vitorias);
+  
   return 0;
 }
